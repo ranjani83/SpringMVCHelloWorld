bfe21855ac8c08b44d8af1cb4de8503f1f5218b7
ranjani83 <ranj.sr@gmail.com>
2016-11-07 17:12:45 +1100

    Add files via upload

---
+++ src/main/java/org/arpit/java2blog/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package org.arpit.java2blog.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/helloworld")
+	public ModelAndView hello() {
+
+		String helloWorldMessage = "Hello world from Spring MVC example!";
+		return new ModelAndView("hello", "message", helloWorldMessage);
+	}
+}
